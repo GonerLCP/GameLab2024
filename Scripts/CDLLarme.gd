47388e2eee837837d10dde56acfab54a0390bab5
GonerLCP <antonin.arnold54700@gmail.com>
2024-04-11 18:09:07 +0200

    IntegrationCommeChloeVeux+PorteQuiChangentSprite

---
+++ Scripts/CDLLarme.gd
@@ -0,0 +1,66 @@
+extends Area2D
+
+var selected = false
+var collisioned = false
+var test : Node2D
+@export var spe_item : SpeItem
+@export var path : Path2D
+var fontained = true
+
+var peutgrab = true
+var mouseOffset
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func followMouse() :
+	position = get_global_mouse_position() + mouseOffset #+ (global_position-get_global_mouse_position())
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+@warning_ignore("unused_parameter")
+func _process(delta):
+	if fontained == true :
+		position = path.position + path.get_child(1).position
+	if selected :
+		fontained = false
+		followMouse()
+	if selected and collisioned :
+		var SoiMeme = $Marker2D.global_position
+		var Lautre = test.get_node("Marker2D").global_position
+		print( test.get_node("SpeItem").numSalle)
+		if test.get_node("SpeItem").numSalle == self.get_node("SpeItem").numSalle and test.get_node("SpeItem").numSalle != 10 :
+			if abs(SoiMeme.x - Lautre.x) <= 20 :
+				if abs(SoiMeme.y - Lautre.y) <= 20:
+					var ObjetACreer = load("res://Nodes/Objets/Cle/"+ self.get_node("SpeItem").NomDeLaCle + ".tscn")
+					var objetTombe = ObjetACreer.instantiate()
+					objetTombe.position = self.position
+					print(objetTombe.position)
+					print(self.global_position)
+					$"..".add_child(objetTombe)
+					self.queue_free()
+					test.queue_free()
+	pass
+
+
+@warning_ignore("unused_parameter")
+func _on_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT :
+		if event.pressed :
+			mouseOffset = position - get_global_mouse_position()
+			selected = true
+		else : 
+			selected = false
+	pass # Replace with function body.
+
+
+func _on_area_entered(area): #faire un cetru ou si on passe une collision on set la visibility à false
+		
+	collisioned = true
+	test = area
+	pass # Replace with function body.
+
+
+@warning_ignore("unused_parameter")
+func _on_area_exited(area):
+	collisioned = false
+	pass # Replace with function body.
--- Scripts/Porte.gd
+++ Scripts/Porte.gd
@@ -1,8 +1,11 @@
 extends StaticBody2D
 
 @export var numPorte : int
+@export var nomSpriteOuvert : String
+var spriteOuvert 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	spriteOuvert = preload("res://Assetts/S6 - Les Champs Elysées/porteS7.png")
 	pass # Replace with function body.
 
 
@@ -10,5 +13,8 @@ func _ready():
 @warning_ignore("unused_parameter")
 func _on_area_2d_body_entered(body): #Si je veux arreter le joueur je devrais mettre un collider autre et pas un area
 	if Global.cleRecup[numPorte] == 1 :
-		self.queue_free()
+		$Sprite2D.texture = spriteOuvert
+		#$CollisionShape2D.disabled = true
+		$CollisionShape2D.set_deferred("disabled", true)
+		#self.queue_free()
 	pass # Replace with function body.
